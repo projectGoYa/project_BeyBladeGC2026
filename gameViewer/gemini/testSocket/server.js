4dd836f1b35c8d1f0bfe03e43d051441c62a2c72
gotooza <510hidepon@gmail.com>
2026-03-16 23:54:06 +0900

    Socket 機能のテスト

---
+++ gameViewer/gemini/testSocket/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
+
+app.use(express.static('public'));
+
+// 起動時にファイルからデータを読み込む
+let kanbanData = { 'todo-list': [], 'doing-list': [], 'done-list': [] };
+if (fs.existsSync('data.json')) {
+    kanbanData = JSON.parse(fs.readFileSync('data.json'));
+}
+
+io.on('connection', (socket) => {
+    socket.emit('sync', kanbanData);
+    socket.on('change', (data) => {
+        kanbanData = data;
+        fs.writeFileSync('data.json', JSON.stringify(kanbanData)); // ファイル保存
+        io.emit('sync', kanbanData); // 全員へ通知
+    });
+});
+
+http.listen(3000, () => console.log('Server running on http://localhost:3000'));
